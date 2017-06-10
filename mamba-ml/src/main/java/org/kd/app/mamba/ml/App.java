90c2752b1add989909f53106d8e92dd7d7caa0b4
KD Singh <kuldeeparora89@gmail.com>
2017-06-10 13:41:59 +0530

    Parent Pom

---
+++ mamba-ml/src/main/java/org/kd/app/mamba/ml/App.java
@@ -0,0 +1,23 @@
+package org.kd.app.mamba.ml;
+
+/**
+ * Created by kuldeep on 10-06-2017.
+ */
+public class App {
+
+
+
+    public static void main(String[] args) {
+
+
+
+
+        RecommendationModel rm = new RecommendationModel();
+
+        rm.build();
+
+
+    }
+
+
+}
